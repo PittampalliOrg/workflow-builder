929dbc65c65e43cfbcfba4281a2405c64f1fb167
Vinod Pittampalli <vinod@pittampalli.com>
2026-04-14 07:22:02 -0400

    Make MCP connection migration idempotent

--- drizzle/0026_mcp_connection_credential_binding.sql
+++ drizzle/0026_mcp_connection_credential_binding.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "mcp_connection"
-ADD COLUMN "connection_external_id" text;
+ADD COLUMN IF NOT EXISTS "connection_external_id" text;
 
 CREATE INDEX IF NOT EXISTS "idx_mcp_connection_connection_external_id"
 ON "mcp_connection" ("connection_external_id");
